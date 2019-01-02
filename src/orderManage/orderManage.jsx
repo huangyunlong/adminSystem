d514ed13bd944788f6e117f458321ed6481aa46a
huangyunlong <1258443294@qq.com>
2019-01-02 17:16:20 +0800

    Update orderManage.jsx

--- src/orderManage/orderManage.jsx
+++ src/orderManage/orderManage.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { observable } from "mobx";
 import { observer } from "mobx-react";
-import { Button, Input, Table, DatePicker } from "antd";
+import { Button, Input, Table, DatePicker,Icon} from "antd";
 import locale from "antd/lib/date-picker/locale/zh_CN";
 import "moment/locale/zh-cn";
 import Mock from "mockjs";
 import "./orderManage.css";
 import tool from "../tools/tool.js";
+const { RangePicker } = DatePicker;
+let publicUrl = "https://sp.tkfun.site/mock/14";
+publicUrl = "http://93.179.103.52:5000";
+let getUrl = publicUrl + "/order/getData";
 @observer
 class OrderManage extends React.Component {
   @observable tableHeight = 0; // 表格高度
   @observable dataSource = []; // 表体
   @observable columns = []; // 表头
-  @observable pagination = {
+  @observable
+  pagination = {
     showSizeChanger: true,
     current: 1,
     pageSize: 10,
@@ -22,96 +27,121 @@ class OrderManage extends React.Component {
     },
     pageSizeOptions: ["10", "20", "50", "100"]
   };
-  @observable loading = false;
-  defineColumns(){
+  @observable loading = true;
+  @observable lastRequestTableParams={};
+  defineColumns() {
     this.columns = [
       {
         title: "订单编号",
-        dataIndex: "orderNumber",
+        dataIndex: "order_no",
+        key: "order_no",
         width: "16%",
-        align: "center"
+        align: "center",
+        sorter: true // 是否可排序
       },
-        {
+      {
         title: "用户名",
-        dataIndex: "userName",
+        dataIndex: "nick_name",
+        key: "nick_name",
         width: "12%",
         align: "center"
       },
       {
         title: "商品详情",
-        dataIndex: "goodDetail",
+        dataIndex: "goodsDetail",
+        key: "goodsDetail",
         align: "center",
         width: "12%",
-        align: "center"
+        render:(text,record)=>(
+          <a href="javascript:;" onClick={this.goodsDetail.bind(this,text,record)}>点击获取详情</a>
+        )
       },
       {
         title: "数量",
         dataIndex: "count",
+        key: "count",
         width: "10%",
         align: "center"
       },
       {
         title: "合计价格",
-        dataIndex: "allPrice",
+        dataIndex: "order_money",
+        key: "order_money",
         width: "12%",
         align: "center"
       },
       {
         title: "实付金额",
-        dataIndex: "price",
+        dataIndex: "pay_money",
+        key: "pay_money",
         width: "12%",
         align: "center"
       },
       {
         title: "付款时间",
-        dataIndex: "paymentTime",
+        dataIndex: "create_time",
+        key: "create_time",
         width: "12%",
-        align: "center"
-      },{
-        title: '状态',
-        dataIndex: "status",
+        align: "center",
+        filterType: "date"        
+      },
+      {
+        title: "状态",
+        dataIndex: "order_status",
+        key: "order_status",
         width: "12%",
         align: "center"
       }
-    ]
-  };
-  async fetchDataSource(params){
+    ];
+    this.columns.forEach(item => {
+      if (item.filterType == "string") {
+        _.merge(item, { ...this.getStringColumnSearchProps(item.title) });
+      } else if (item.filterType == "date") {
+        _.merge(item, { ...this.getDateColumnSearchProps(item.title) });
+      }
+    });
+  }
+  goodsDetail(text,record){
+    console.log(1);
+    alert(record.order_no)
+  }
+  async fetchDataSource(params) {
     this.loading = true;
-    console.log(params)
-    let getUrl = 'http://93.179.103.52:5000/tables/order';
-    // let response = await tool.requestAjaxSync('/tables/order', "GET", {
-    //   ...params
-    // });
-    let data = await tool.requestAjaxSync(getUrl, "post", {
+    let data = await tool.requestAjaxSync(getUrl, "POST", {
       getTableDataParams: params
     });
-    console.log(data)
+    this.loading = false;
+    console.log("data");
+    console.log(data);
     data = data.data;
-    console.log('data');
-    // this.pagination.total = data.length;
-    // this.loading = false;
-    let list = Mock.mock({
-      "list|10-100": [
-        {
-          "key|+1": 0,
-          orderNumber: "@id(5)",
-          userName: "@cname",
-          invoiceName: "@cname",
-          taxNumber: "@id",
-          email: "@email",
-          goodDetail:'@ctitle',
-          count: '@integer(1,10)',
-          price: '@integer(1,10)',
-          paymentTime:'@datetime("yyyy-MM-dd HH:mm:ss")',
-          "allPrice":'@integer(60,1000)',
-          "status|1": ['已付款','已使用']
-        }
-      ]
-    }).list;
-    this.dataSource = _.clone(list);
-    this.pagination.total = list.length;
-    this.pagination.page = _.clone(params.page);
-    this.pagination.pageSize = _.clone(params.pageSize);
+    let list = data.datas;
+    // let list = Mock.mock({
+    //   "list|10-100": [
+    //     {
+    //       "key|+1": 0,
+    //       orderNumber: "@id(5)",
+    //       userName: "@cname",
+    //       invoiceName: "@cname",
+    //       taxNumber: "@id",
+    //       email: "@email",
+    //       goodDetail: "@ctitle",
+    //       count: "@integer(1,10)",
+    //       price: "@integer(1,10)",
+    //       paymentTime: '@datetime("yyyy-MM-dd HH:mm:ss")',
+    //       allPrice: "@integer(60,1000)",
+    //       "status|1": ["已付款", "已使用"]
+    //     }
+    //   ]
+    // }).list;
+    this.dataSource = list.map(item => {
+      return {
+        key: item.id,
+        ...item
+      };
+    });
+    this.pagination.total = data.tableInfo.total;
+    this.pagination = _.clone(this.pagination);
+    this.loading = false;
   }
   initTable() {
     this.defineColumns();
@@ -124,51 +154,93 @@ class OrderManage extends React.Component {
     this.initTable();
   }
   componentDidMount() {
-    setTimeout(()=>{
-      this.tableHeight = this.refs.orderManage.offsetHeight - 110 - 90;
-    },200)
-  }
-  onStartChange(value, dateString) {
-    console.log("Formatted Selected start Time: ", dateString);
-  }
-  onEndChange(value, dateString) {
-    console.log("Formatted Selected end Time: ", dateString);
+    setTimeout(() => {
+      this.tableHeight = this.refs.orderManage.offsetHeight - 110 - 40;
+    }, 200);
   }
   async onhandleTableChange(pagination, filters, sorter) {
-    this.pagination = _.merge(this.pagination,pagination);
+    this.pagination = _.merge(this.pagination, pagination);
     await this.fetchDataSource({
       pageSize: pagination.pageSize,
-      page: pagination.current
+      page: pagination.current,
+      sortField: sorter.field,
+      sortOrder: sorter.order,
+      filters
     });
   }
+
+  getDateColumnSearchProps = searchTitle => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters
+    }) => (
+      <div className="custom-filter-dropdown-date">
+        <RangePicker
+          onChange={(date, dateString) => {
+            setSelectedKeys(dateString ? [dateString] : []);
+            confirm();
+          }}
+          style={{ width: 250, marginBottom: 8, display: "block" }}
+        />
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? "#1890ff" : undefined }} />
+    )
+  });
+
+  getStringColumnSearchProps = searchTitle => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters
+    }) => (
+      <div className="custom-filter-dropdown-string">
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`搜索 【${searchTitle}】`}
+          value={selectedKeys[0]}
+          onChange={e =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => confirm()}
+          style={{ width: 188, marginBottom: 8, display: "block" }}
+        />
+        <Button
+          type="primary"
+          onClick={() => confirm()}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          搜索
+        </Button>
+        <Button
+          onClick={() => clearFilters()}
+          size="small"
+          style={{ width: 90 }}
+        >
+          重置
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    }
+  });
   render() {
     return (
       <div className="orderManage" ref="orderManage">
         <h2>订单管理</h2>
-        <div className="orderManage_search">
-          <div className="searchConditionsInput">
-            <div className="search_input">
-              <Input placeholder="输入订单编号" />
-              <DatePicker
-                showTime
-                format="YYYY-MM-DD HH:mm:ss"
-                locale={locale}
-                placeholder="请选择开始时间"
-                onChange={this.onStartChange}
-              />
-              <DatePicker
-                showTime
-                format="YYYY-MM-DD HH:mm:ss"
-                locale={locale}
-                placeholder="请选择结束时间"
-                onChange={this.onEndChange}
-              />
-            </div>
-            <div className="btn">
-              <Button type="primary">确定</Button>
-            </div>
-          </div>
-        </div>
         <div className="orderManage_tables">
           <div className="orderManage_tableContent">
             <Table
@@ -179,6 +251,7 @@ class OrderManage extends React.Component {
               dataSource={this.dataSource}
               onChange={this.onhandleTableChange.bind(this)}
               pagination={this.pagination}
+              loading={this.loading}
             />
           </div>
         </div>
